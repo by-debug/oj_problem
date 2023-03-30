58e04ae44ded006e928b62e16b813b59e87c9788
by-debug <by20051002@gmail.com>
2023-03-31 03:10:06 +0800

    TIOJ 1987(with bug)

--- tioj/1687.cpp
+++ tioj/1687.cpp
@@ -56,16 +56,85 @@ using namespace std;
 
 const double eps = 1e-9;
 
-int n,q;
+int n,q,ans=0;
+vvi G,P;
+vi link,lev;
+
+void dfs(int x,int level)
+{
+	if (~lev[x])
+		return;
+	link[level]=x;
+	lev[x]=level;
+	for (int i=0;(1<<i)<level;++i)
+		P[x][i]=link[level-(1<<i)];
+	for (auto i:G[x])
+		dfs(i,level+1);
+	link[level]=-1;
+}
+
+int find_ancestor(int x,int count)
+{
+	if(count = 0)
+	{
+		return x;
+	}
+	else
+	{
+		int c = 1;
+		while ((1<<(c+1))<count)
+			++c;
+		return find_ancestor(P[x][c],count-(1<<c));
+	}
+}
+
+int lca(int a,int b)
+{
+	if (lev[a]<lev[b])
+		swap(a,b);
+	int a1 = find_ancestor(a,lev[a]-lev[b]);
+	int l=0,r=lev[a1];
+	while (l!=r)
+	{
+		int m = l+(r-l)/2;
+		if (find_ancestor(a1,m)==find_ancestor(b,m))
+			l = m;
+		else
+			r = m;
+	}
+	return l;
+}
 
 signed main()
 {
 	settings();
     cin >> n >> q;
-	for (int i=0;i<n;++i)
+	G.assign(n,vi());
+	P.assign(n,vi(__lg(n)+1));
+	link.assign(n,-1);
+	lev.assign(n,-1);
+	for (int i=0;i<n-1;++i)
     {
         int a,b;
         cin >> a >> b;
-    }
+		G[a-1].pb(b-1);
+		G[b-1].pb(a-1);
+	}
+	dfs(0,0);
+	for (int i=0;i<q;++i)
+	{
+		int s,t,k;
+		cin >> s >> t >> k;
+		s -= 1;
+		t -= 1;
+		int L = lca(s,t);
+		if (k<lev[s]-lev[L])
+			ans = find_ancestor(s,k);
+		else if (lev[s]+lev[t]-2*lev[L]>=k)
+			ans = find_ancestor(s,lev[s]+lev[t]-2*lev[L]-k);
+		else
+			ans = -1;
+	}
+	cout << ans << '\n';
 	return 0;
 }
